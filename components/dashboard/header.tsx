08317fdbd9506e766a77f6dbf8be4e810ea33492
Dipanshu Verma <dipanshuverma1247@gmail.com>
2026-06-03 15:00:09 +0530

    Revert "Dashboard Optimise"
    This reverts commit c08619d0f83996263ca8b0dc2e3656e78808d432.

--- components/dashboard/header.tsx
+++
@@ -1,9 +0,0 @@
-export function DashboardHeader({ user }: { user: any }) {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-        Welcome {user.name ?? "Guest"} 👋
-      </h1>
-    </div>
-  );
-}
